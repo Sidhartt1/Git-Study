2c32d4f35884bee89f0a34e2f7558ba65de877a9
Sidhartt1 <sidhsidhad@gmail.com>
2022-11-24 16:44:51 +0530

    department controller

--- Spring-Crud/src/main/java/com/example/demo/Controller/DepartmentController.java
+++ Spring-Crud/src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -3,14 +3,29 @@ package com.example.demo.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Department;
+import com.example.demo.service.DepartmentService;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController 
 {
+	@Autowired
+	private DepartmentService service;
+	
+	@PostMapping("/create")
+	public void createDepartment(@RequestBody Department department)
+	{
+		service.createDepartment(department);
+	}
+	
+	
 	
 	
 }
--- Spring-Crud/src/main/java/com/example/demo/service/DepartmentService.java
+++ Spring-Crud/src/main/java/com/example/demo/service/DepartmentService.java
@@ -1,9 +1,21 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.DepartmentRepo;
+import com.example.demo.model.Department;
 
 @Service
 public class DepartmentService
 {
+	
+	@Autowired
+	private DepartmentRepo repo;
+
+	public void createDepartment(Department department) 
+	{
+		repo.save(department);
+	}
 	
 }
