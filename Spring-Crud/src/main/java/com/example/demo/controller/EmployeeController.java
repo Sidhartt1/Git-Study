d99d6a6f5151e597cbe4c0754c0729f13938f45e
prathijnaudupa <prathijnau@gmail.com>
2022-11-24 13:02:27 +0530

    create operation is done

--- Spring-Crud/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Spring-Crud/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+
+
+
 public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
+	@PostMapping("/employees")
+	public void addEmployees(@RequestBody Employee employee){
+		employeeService.addEmployee(employee);
+	}
+
+	
+	
 
 }
--- Spring-Crud/src/main/java/com/example/demo/service/EmployeeService.java
+++ Spring-Crud/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.EmployeeDao;
+import com.example.demo.model.Employee;
+
+
 public class EmployeeService {
+	
+	private EmployeeDao employeedao;
+	
+	public void addEmployee(Employee e) {
+		employeedao.saveAll(e);
+	}
 
 }
