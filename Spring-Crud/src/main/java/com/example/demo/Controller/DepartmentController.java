8aef8237cefe1ce934a6314c87f986ef7553b6b6
Sidhartt1 <sidhsidhad@gmail.com>
2022-11-24 16:35:34 +0530

    new commit

--- Spring-Crud/src/main/java/com/example/demo/Controller/DepartmentController.java
+++ Spring-Crud/src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -1,5 +1,9 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/department")
 public class DepartmentController 
 {
+	
 	
 }
