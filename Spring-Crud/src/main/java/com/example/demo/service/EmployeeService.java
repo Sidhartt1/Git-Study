83f5b22f7080ef1512c22ed3793fa88f335bbf20
SWATHI <swathi@specrio.co>
2022-11-24 13:12:37 +0530

    changes

--- Spring-Crud/src/main/java/com/example/demo/service/EmployeeService.java
+++ Spring-Crud/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,5 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Employee;
+
 public class EmployeeService {
+	
+	public void updateEmployee(Employee emp, int id){
+		if(id == emp.getEmpid()) {
+			employeeRepository.save(emp);
+		}
+	}
 
 }
